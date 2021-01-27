c762b1c27e1bdad82ca1ac9068637d2284e87141
alex chow <zhouchuanglin@gmail.com>
2021-01-27 13:07:33 +0100

    添加项目文件。

---
+++ FreeSqlDemos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FreeSqlDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

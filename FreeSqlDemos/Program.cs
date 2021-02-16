41d85224993864cf0316b2777ee97a8e8fa87859
Alex Chow <zhouchuanglin@gmail.com>
2021-02-16 15:55:24 +0100

    添加Console例子

--- FreeSqlDemos/Program.cs
+++ FreeSqlDemos/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using FreeSql.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 
 namespace FreeSqlDemos
 {
@@ -7,6 +11,48 @@ namespace FreeSqlDemos
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var fsql = new FreeSql.FreeSqlBuilder()
+            .UseConnectionString(FreeSql.DataType.Sqlite, "Data Source=document.db; Pooling=true;Min Pool Size=1")
+            .UseAutoSyncStructure(true) //自动同步实体结构【开发环境必备】
+            .UseMonitorCommand(cmd => Console.Write(cmd.CommandText))
+            .Build();
+
+            var ItemList = new List<Item>()
+            {
+                new Item {  Text = "假装 First item", Description="This is an item description." },
+                new Item {  Text = "的哥 Second item", Description="This is an item description." },
+                new Item { Text = "四风 Third item", Description="This is an item description." },
+                new Item {  Text = "加州 Fourth item", Description="This is an item description." },
+                new Item { Text = "阳光 Fifth item", Description="This is an item description." },
+                new Item {  Text = "孔雀 Sixth item", Description="This is an item description." }
+            };
+
+            if (fsql.Select<Item>().Count() == 0)
+            {
+                fsql.Insert<Item>().AppendData(ItemList).ExecuteAffrows();
+            }
+            ItemList = fsql.Select<Item>().ToList();
+
+
+            Console.WriteLine("ItemList: " + ItemList.Count());
+
+
         }
     }
+
+
+    public class Item
+    {
+        [Column(IsIdentity = true)]
+        [DisplayName("序号")]
+        public int Id { get; set; }
+
+        [DisplayName("名称")]
+        public string Text { get; set; }
+
+        [DisplayName("描述")]
+        public string Description { get; set; }
+    }
+
 }

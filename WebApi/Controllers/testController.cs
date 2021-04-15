a157bad95c860f55842064e9195a09ddf98bf095
Alex Chow <zhouchuanglin@gmail.com>
2021-04-15 13:14:15 +0200

    添加WebApi demo, 修复前一版apps没成功上传

---
+++ WebApi/Controllers/testController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static WebApi.Startup;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class testController : ControllerBase
+    {
+
+        IFreeSql _fsql { get; set; }
+        private readonly ILogger<testController> _logger;
+
+        public testController(ILogger<testController> logger, IFreeSql fsql)
+        {
+            _logger = logger;
+            _fsql = fsql;
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> Get()
+        {
+            var ItemList = _fsql.Select<Item>().ToList();
+
+            return ItemList;
+        }
+
+        [HttpPut]
+        public IEnumerable<Item> Put(string name)
+        {
+            var id = _fsql.Insert<Item>().AppendData(new Item() { Text =name}).ExecuteIdentity();
+            var ItemList = _fsql.Select<Item>().Where (a=>a.Id==id).ToList();
+            return ItemList;
+        }
+    }
+}
---
+++ WebApi/Startup.cs
@@ -0,0 +1,108 @@
+using FreeSql.DataAnnotations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi
+{
+    public class Startup
+    {
+
+
+        public class Item
+        {
+            [Column(IsIdentity = true)]
+            [DisplayName("序号")]
+            public int Id { get; set; }
+
+            [DisplayName("名称")]
+            public string Text { get; set; }
+
+            [DisplayName("描述")]
+            public string Description { get; set; }
+        }
+
+
+
+        IFreeSql fsql = new FreeSql.FreeSqlBuilder()
+            .UseConnectionString(FreeSql.DataType.Sqlite, "Data Source=document.db; Pooling=true;Min Pool Size=1")
+            .UseAutoSyncStructure(true) //自动同步实体结构【开发环境必备】
+            .UseMonitorCommand(cmd => Console.Write(cmd.CommandText))
+            .Build();
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            //初始化demo数据
+
+            var ItemList = new List<Item>()
+            {
+                new Item {  Text = "假装 First item", Description="This is an item description." },
+                new Item {  Text = "的哥 Second item", Description="This is an item description." },
+                new Item { Text = "四风 Third item", Description="This is an item description." },
+                new Item {  Text = "加州 Fourth item", Description="This is an item description." },
+                new Item { Text = "阳光 Fifth item", Description="This is an item description." },
+                new Item {  Text = "孔雀 Sixth item", Description="This is an item description." }
+            };
+
+            if (fsql.Select<Item>().Count() == 0)
+            {
+                fsql.Insert<Item>().AppendData(ItemList).ExecuteAffrows();
+            }
+            ItemList = fsql.Select<Item>().ToList();
+
+
+            Console.WriteLine("ItemList: " + ItemList.Count());
+            
+            
+            
+            services.AddSingleton(fsql);
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}

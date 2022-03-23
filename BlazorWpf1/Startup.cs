79ed93815e61d45f09c20d41f953522e1667b825
Alex Chow <zhouchuanglin@gmail.com>
2022-03-23 23:58:13 +0100

    添加 [BlazorWpf] 和 [BlazorWinForms]

---
+++ BlazorWpf1/Startup.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BlazorWpf1
+{
+    public static class Startup
+    {
+        public static IServiceProvider? Services { get; private set; }
+
+        public static void Init()
+        {
+            var host = Host.CreateDefaultBuilder()
+                           .ConfigureServices(WireupServices)
+                           .Build();
+            Services = host.Services;
+        }
+
+        private static void WireupServices(HostBuilderContext context, IServiceCollection services)
+        {
+            services.AddBlazorWebView();
+        }
+    }
+}
---
+++ Maui/BlazorWinForms1/Index.razor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryShared;
+ 
+
+namespace BlazorWinForms1
+{
+    public partial class Index
+    {
+        List<TaskItem> ItemList=new List<TaskItem>();
+        DataService dataService;
+        string CounterLabel  = "Current count: 0";
+        TaskItem ItemSelected;
+        int count = 0;
+        protected override void OnInitialized()
+        {
+            dataService = new DataService();
+            ItemList = dataService.Init(initdemodatas: true);
+
+        }
+
+        private void OnCounterClicked()
+        {
+            count = ItemList.Count;
+            CounterLabel = $"Current count: {count}";
+
+        }
+        private void OnSelectOneClicked()
+        {
+            ItemSelected = null;// ItemList[3];
+            CounterLabel = $"Select item: {ItemSelected}";
+
+        }
+
+        private void OnAddClicked()
+        {
+            var item = dataService.Add($"New {count + 1}");
+            ItemList.Add(item);
+
+            OnCounterClicked();
+        }
+        private void OnRefreshClicked()
+        {
+            ItemList.Clear();
+            foreach (var item in dataService.Init())
+            {
+                this.ItemList.Add(item);
+            }
+            OnCounterClicked();
+        }
+
+        private void OnModifyClicked()
+        {
+            ItemList[0] = dataService.Modify() ?? ItemList[0];
+            OnCounterClicked();
+        }
+
+        private void OnDeleteClicked()
+        {
+            if (dataService.Delete()) ItemList.RemoveAt(ItemList.Count - 1);
+            OnCounterClicked();
+        }
+    }
+}
